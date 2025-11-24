33c1d032a9aad6b5947131767050593cfcfe8b8e
verpivia-1 <cyrr0070088@gmail.com>
2025-11-24 17:40:26 +0900

    난별이싫어

---
+++ HOME/hatestar.cs
@@ -0,0 +1,48 @@
+﻿namespace HOME
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1
+            for(int i =1; i<=5; i++)
+            {
+                Console.WriteLine();
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+            */
+
+            /* 2
+            for (int i = 1; i <=5; i++)
+            {
+                Console.WriteLine();
+
+                for(int j = 5; j>=i; j--)
+                {
+                    Console.Write("*");
+                }
+            
+            }
+            */
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int a = i;
+                int b = a % 2;
+               
+                if (b == 1)
+                {
+                    Console.WriteLine("{0}는 홀수 입니다.", a);
+                }
+                else
+                {
+                    Console.WriteLine("{0}는 짝수 입니다.", a);
+                }
+
+            }
+
+         }
+    }
+}
