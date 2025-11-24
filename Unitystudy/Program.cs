e6bc3650fd5f2c87644a61a1c05492c35fe27e59
verpivia-1 <cyrr0070088@gmail.com>
2025-11-24 14:45:42 +0900

    init

---
+++ Unitystudy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Unitystudy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
